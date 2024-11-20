5b93889d0a37bd9834fb21bc865391dd6708a4d8
ngoctram15 <Tramnguyen1516@gmail.com>
2024-11-21 00:26:48 +0900

    udated

--- static/app.js
+++ static/app.js
@@ -1,8 +1,40 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id; 
+  const editInput = prompt("수정할 값을 입력해주세요");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT", 
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editInput,
+    }),
+  });
+}
+
+const delBtn = document.createElement("button");
+delBtn.innerText = "삭제";
+delBtn.addEventListener("click", deleteMemo);
+delBtn.dataset.id = memo.id;
+
+
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
+
   const li = document.createElement("li");
   li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click")
+  editBtn.dataset.id = memo.id;
+
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
   ul.appendChild(li);
+  
 }
 
 async function readMemo() {
@@ -26,6 +58,15 @@ async function creatMemo(value) {
   });
   readMemo();
 }
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id; 
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+
 
 function handleSubmit(event) {
   event.preventDefault();
--- static/main.py
+++ static/main.py
@@ -19,4 +19,20 @@ def create_memo(memo: Memo):
 def read_memo():
     return memos    
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos: 
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content
+            return '성공했습니다'
+    return '그런 메모가 없다'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    for index, memo in enumerate(memos):
+        if memo.id == memo_id:
+            memos.pop(index)
+            return '성공했습니다'
+    return '그런 메모가 없다'
+            
 app.mount("/", StaticFiles(directory="static",html=True),name="static")
